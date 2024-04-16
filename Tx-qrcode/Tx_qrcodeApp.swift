e545fe66927247c95ee4b5744427796e7967fa70
ZXC-zxc <qinhuanle0805@gmail.com>
2024-04-16 15:01:04 +0800

    Initial Commit

---
+++ Tx-qrcode/Tx_qrcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tx_qrcodeApp.swift
+//  Tx-qrcode
+//
+//  Created by xiaochen on 2024/4/16.
+//
+
+import SwiftUI
+
+@main
+struct Tx_qrcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
